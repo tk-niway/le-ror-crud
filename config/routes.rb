ea20d64dd5e9e7d54c056e3a200aa6556022c8e9
tk_niway <urban.hang274@gmail.com>
2023-12-30 09:42:41 +0900

    added Articles and modified routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'articles#index'
+  # get 'article/index'
+  # get 'article/show'
+  # get 'article/new'
+  # get 'article/edit'
+  resources :articles
+  # resources :articles, only: [:index, :show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
