01c54b285b40ed170f2e97ae9097463d4440fcd4
tk_niway <urban.hang274@gmail.com>
2023-12-30 11:31:55 +0900

    implemented create article and show an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,28 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    end
   end
 
   def edit
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
